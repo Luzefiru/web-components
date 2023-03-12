dfd6f323c2af45ccf7a0196032c89eb951b66eaa
Luzefiru <ian.deejay.iv@gmail.com>
2023-03-12 15:57:30 +0800

    fix: remove debug statement

--- giphy-api-demo/src/script.js
+++ giphy-api-demo/src/script.js
@@ -8,7 +8,6 @@ btn.addEventListener('click', () => {
 
 async function generateImage() {
   try {
-    console.log("hi");
     const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=ZZOwgGEMXkUJLe2q1C0yhESYDGYhrOrI&s=cats&weirdness=10', { mode: 'cors' });
     const results = await response.json();
     img.removeAttribute('src');
