090b61f4da7fe3c77684125b428f8b09b2ac99b0
Luzefiru <ian.deejay.iv@gmail.com>
2023-03-12 15:56:30 +0800

    feat: refactor code to use async/await instead of Promise methods .then() & .catch()

---
+++ giphy-api-demo/src/script.js
@@ -0,0 +1,19 @@
+const img = document.querySelector('img');
+generateImage();
+    
+const btn = document.querySelector('button');
+btn.addEventListener('click', () => {
+  generateImage();
+})
+
+async function generateImage() {
+  try {
+    console.log("hi");
+    const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=ZZOwgGEMXkUJLe2q1C0yhESYDGYhrOrI&s=cats&weirdness=10', { mode: 'cors' });
+    const results = await response.json();
+    img.removeAttribute('src');
+    img.setAttribute('src', results['data']['images']['original']['url']);
+  } catch (error) {
+    console.log('An error occured: ', error);
+  }
+}
